db30a1142b87104d9b638c7fff0e85e5b2682e35
Dios <abhishekpatawari.codes@gmail.com>
2024-07-11 09:37:33 +0530

    add: calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n, q;
+    cout << "num 1: " <<'\n';
+    cin >> n ;
+    cout << "num 2: "  << '\n';
+    cin >> q;
+   
+   cout << "add: " << n+q << '\n';
+   cout << "sub: " << n-q << '\n';
+   cout << "mul: " << n*q << '\n';
+   cout << "div: " << n/q << '\n';
+   
+   
+    return 0;
+    
+}
