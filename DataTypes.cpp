7eb027f80f98aa952ab13ee13f51ea64f593ec0b
Dios <abhishekpatawari.codes@gmail.com>
2024-07-11 10:02:34 +0530

    data types

---
+++ DataTypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    std::cout << "Size of char: " << sizeof(char) << " byte(s)" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << " byte(s)" << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << " byte(s)" << std::endl;
+    std::cout << "Size of double: " << sizeof(double) << " byte(s)" << std::endl;
+    std::cout << "Size of bool: " << sizeof(bool) << " byte(s)" << std::endl;
+    
+    return 0;
+}
